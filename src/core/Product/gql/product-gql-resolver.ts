75b026e6c95759857c9a5be1692bb0ba4098c9bc
Atlas <btw210899@gmail.com>
2023-04-18 11:23:28 +0300

    feat: added graphql mutation

--- src/core/Product/gql/product-gql-resolver.ts
+++ src/core/Product/gql/product-gql-resolver.ts
@@ -1,18 +1,25 @@
-import { Args, Int, Query, Resolver } from '@nestjs/graphql'
+import { Args, Int, Mutation, Query, Resolver } from '@nestjs/graphql'
 import { ProductGQLType } from './types/product-gql-type'
 import { ProductService } from '../product.service'
+import { CreateProductGQLInput } from './inputs/create-product-gql-input'
 
 @Resolver(() => ProductGQLType)
 export class ProductGQLResolver {
 	constructor(private productService: ProductService) {}
 
 	@Query(() => [ProductGQLType])
-	async productsList() {
+	productsList() {
 		return this.productService.getProducts()
 	}
 
 	@Query(() => ProductGQLType)
-	async product(@Args('id', { type: () => Int }) id: number) {
+	product(@Args('id', { type: () => Int }) id: number) {
 		return this.productService.findProductById(id)
+	}
+
+	@Mutation(() => ProductGQLType)
+	async createProduct(@Args('productInput') productInput: CreateProductGQLInput) {
+		const product = await this.productService.createProduct(productInput)
+		return product
 	}
 }
