51eeb32e85869c1d55e6fa65d5b5b3713e90010f
Atlas <btw210899@gmail.com>
2023-05-14 19:36:30 +0300

    feat: added user tests and some refactoring

--- src/core/user/repository/abstract.user.repository.ts
+++ src/core/user/repository/abstract.user.repository.ts
@@ -1,5 +1,5 @@
-import { User as UserModel } from '@prisma/client'
 import { User } from '../domain/user.entity'
+import { Role } from '../domain/user-types'
 
 export type searchOptions = {
 	limit?: number
@@ -7,6 +7,17 @@ export type searchOptions = {
 	order?: 'asc' | 'desc'
 	email?: string
 	phoneNumber?: string
+}
+
+export type UserModel = {
+	id: number
+	email: string
+	phoneNumber: string
+	firstName: string
+	lastName: string
+	password: string
+	role: Role
+	isConfirmed: boolean
 }
 
 export abstract class AbstractUserRepository {
